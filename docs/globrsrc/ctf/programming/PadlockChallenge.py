4be13901458acdfc2be30934d1c4b8f49a0cbece
raging-loon <cxmacolley@gmail.com>
2022-08-18 19:43:50 -0400

    added highlightjs framework

---
+++ docs/globrsrc/ctf/programming/PadlockChallenge.py
@@ -0,0 +1,14 @@
+# Author: Dielle De Noon
+# Date: 8/18/22 (18/8/22)
+
+print("Welcome to the padlock challenge")
+code = input("Enter the combination")
+#Update the code below to solve challenge
+
+if code == "928":
+
+    print("Congradulations!!! You have unlocked the code")
+else:
+    print("Invalid Combination")
+    code = input("Enter the Combination")
+
--- docs/js.js
+++ docs/js.js
@@ -2,12 +2,8 @@
 // nice
 function change_to_visible(component){
   var x = document.getElementById(component);
-  if(x.classList.contains("clicked")){
-    x.style.visibility = 'hidden';
-    x.classList.remove('clicked')
-  }
-  else {
+  if(x.style.visibility == 'hidden')
     x.style.visibility = 'visible';
-    x.classList.add("clicked")
-  }
+  else
+    x.style.visibility = 'hidden';
 }
