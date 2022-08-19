4d1bc5d3401cf27834afe2b326f45f7ec18ea056
raging-loon <cxmacolley@gmail.com>
2022-08-18 20:12:13 -0400

    added pdlck1

--- docs/js.js
+++ docs/js.js
@@ -2,8 +2,12 @@
 // nice
 function change_to_visible(component){
   var x = document.getElementById(component);
-  if(x.style.visibility == 'hidden')
-    x.style.visibility = 'visible';
-  else
+  if(x.classList.contains("clicked")){
     x.style.visibility = 'hidden';
+    x.classList.remove('clicked')
+  }
+  else {
+    x.style.visibility = 'visible';
+    x.classList.add("clicked")
+  }
 }
